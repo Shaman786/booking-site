9757d85d179aefda2f0efa1acdcbc119123d14f6
Shaman786 <57454306+Shaman786@users.noreply.github.com>
2023-06-02 18:03:27 +0530

    gi

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerList">
+        <div className="headerListItems"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
-import Navbar from '../../components/Navbar/Navbar'
-import './home.css'
+import Navbar from "../../components/Navbar/Navbar";
+import "./home.css";
+import Header from "../../components/header/Header";
 
 export const Home = () => {
   return (
-    <div><Navbar/></div>
-  )
-}
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Header />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
