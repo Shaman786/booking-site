6c9ab16cbf0bbaec508553d0ff9b12f6527c2ccd
Fayaj Hossain <57454306+Shaman786@users.noreply.github.com>
2023-06-03 20:29:08 +0000

    did same changes to directory

--- api/index.js
+++ api/index.js
@@ -1,2 +1,29 @@
 import express from "express";
-const express = req
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected!");
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("Server is listening on port 8800");
+});
