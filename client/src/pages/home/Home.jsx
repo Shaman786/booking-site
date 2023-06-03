1663a324b7baff8bf315d048645e5b2c3105e0b7
shaman <fayazhossain88@gmail.com>
2023-06-03 16:33:14 +0530

    working on Featured component

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,17 +1,17 @@
 import Navbar from "../../components/Navbar/Navbar";
 import "./home.css";
 import Header from "../../components/header/Header";
+import Featured from "../../components/featured/Featured";
 
 export const Home = () => {
   return (
-    <>
-      <div>
-        <Navbar />
+    <div>
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
       </div>
-      <div>
-        <Header />
-      </div>
-    </>
+    </div>
   );
 };
 
