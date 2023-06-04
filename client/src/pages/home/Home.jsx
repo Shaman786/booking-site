b2088591f3be0feab28ea2cbc5e382792aea4ef1
Fayaj Hossain <57454306+Shaman786@users.noreply.github.com>
2023-06-04 21:35:28 +0000

    client updated

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,11 @@
-import Navbar from "../../components/Navbar/Navbar";
-import "./home.css";
-import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
-import MailList from "../../components/mailList/MailList";
 import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import MailList from "../../components/mailList/MailList";
+import Navbar from "../../components/navbar/Navbar";
 import PropertyList from "../../components/propertyList/PropertyList";
+import "./home.css";
 
 const Home = () => {
   return (
@@ -16,7 +16,7 @@ const Home = () => {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
-        <h1 className="homeTitle">Home guests love</h1>
+        <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties />
         <MailList />
         <Footer />
